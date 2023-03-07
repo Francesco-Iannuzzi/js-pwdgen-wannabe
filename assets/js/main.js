105b305ffe4d8e7cdd1698a7ca917f39e4edb2c0
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-07 14:52:45 +0100

    initial commit (HTML + JS) + add trace and steps

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// chiedere all'utente il suo nome, cognome ed il colore preferito e mostrare le risposte in schermata
+
+//inserire un prompt per chiedere all'utente il suo nome, registrandolo in una variabile
+let username = prompt ("Qual è il tuo nome?")
+console.log(username);
+
+//inserire un prompt per chiedere all'utente il suo cognome, registrandolo in una variabile
+let surname = prompt ("Qual è il tuo cognome?")
+console.log(surname);
+
+//inserire un prompt per chiedere all'utente il suo colore preferito, registrandolo in una variabile
+let favoriteColor = prompt ("Qual è il tuo cognome?")
+console.log(favoriteColor);
+
+//aggiungere un h1 con id per mostrare in schermata le risposte
+
+//stampare in schermata le risposte tutte attaccate con l'aggiunta del suffisso '23'
