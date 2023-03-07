71d6eb78310c5d4a53f48ef8c9a4a56583617402
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-07 15:49:40 +0100

    fix indentetion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
 // Traccia: chiedere all'utente il suo nome, cognome ed il colore preferito e mostrare le risposte in schermata
 
 //inserire un prompt per chiedere all'utente il suo nome, registrandolo in una variabile
-const username = prompt ("What's your name?");
+const username = prompt("What's your name?");
 console.log(username);
 
 //inserire un prompt per chiedere all'utente il suo cognome, registrandolo in una variabile
-const surname = prompt ("What's your surname");
+const surname = prompt("What's your surname");
 console.log(surname);
 
 //inserire un prompt per chiedere all'utente il suo colore preferito, registrandolo in una variabile
-const favoriteColor = prompt ("What's your favorite color?");
+const favoriteColor = prompt("What's your favorite color?");
 console.log(favoriteColor);
 
 //aggiungere un h1 con id per mostrare in schermata le risposte
@@ -19,4 +19,4 @@ const nameElement = document.getElementById('answers');
 console.log(nameElement);
 
 //stampare in schermata le risposte tutte attaccate con l'aggiunta del suffisso '23' assegnando un nuovo valore all'innerHTML
-nameElement.innerHTML=`${username}${surname}${favoriteColor}23`;
+nameElement.innerHTML = `${username}${surname}${favoriteColor}23`;
