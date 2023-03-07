6c8f88fd938fe449e4d473e8a5a4ffee8153bbd7
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-03-07 15:17:56 +0100

    add stamp of answres for client

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
 // chiedere all'utente il suo nome, cognome ed il colore preferito e mostrare le risposte in schermata
 
 //inserire un prompt per chiedere all'utente il suo nome, registrandolo in una variabile
-let username = prompt ("Qual è il tuo nome?")
+let username = prompt ("Qual è il tuo nome?");
 console.log(username);
 
 //inserire un prompt per chiedere all'utente il suo cognome, registrandolo in una variabile
-let surname = prompt ("Qual è il tuo cognome?")
+let surname = prompt ("Qual è il tuo cognome?");
 console.log(surname);
 
 //inserire un prompt per chiedere all'utente il suo colore preferito, registrandolo in una variabile
-let favoriteColor = prompt ("Qual è il tuo cognome?")
+let favoriteColor = prompt ("Qual è il tuo cognome?");
 console.log(favoriteColor);
 
 //aggiungere un h1 con id per mostrare in schermata le risposte
 
-//stampare in schermata le risposte tutte attaccate con l'aggiunta del suffisso '23'
+//seleziono e salvo in una variabile l'elemento della dom con getElementById
+let nameElement = document.getElementById('answers');
+console.log(nameElement);
+
+//stampare in schermata le risposte tutte attaccate con l'aggiunta del suffisso '23' assegnando un nuovo valore all'innerHTML
+nameElement.innerHTML=`${username}${surname}${favoriteColor}23`;
